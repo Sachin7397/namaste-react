3247ea57299062ce64d549d0628160a326290e76
Sachin7397 <sachinskamble0910@gmail.com>
2024-03-09 19:22:41 +0530

    changes

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ root.render(heading)
 const parent = React.createElement("div",{id:"parent"},[React.createElement("div",{id:"child"},[React.createElement("h1",{},"I am h1 tag"),React.createElement("h2",{},"I am h2 tag")]),
 React.createElement("div",{id:"child2"},[React.createElement("h1",{},"I am h1 tag"),React.createElement("h2",{},"I am h2 tag")])])
 
+
 root.render(parent)
 
 console.log('parent', parent)
